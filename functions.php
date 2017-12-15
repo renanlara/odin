72042f8820a60c8e432b7ed237c22c854fb0bab4
Renan Lara <contato@renanlara.com.br>
2017-12-15 14:22:31 -0200

    Allow SVG through WordPress Media Uploader

--- functions.php
+++ functions.php
@@ -506,3 +506,9 @@ if ( ! function_exists( 'social_media' ) ) {
 	}
 }
 
+// Allow SVG through WordPress Media Uploader
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
