0a7f4b2ad304d94fe0ade945356351591e67631b
Renan Lara <contato@renanlara.com.br>
2017-12-18 10:02:50 -0200

    NEW FUNCTION -> Hide editor on Home Page

--- functions.php
+++ functions.php
@@ -512,3 +512,29 @@ function cc_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+ * Hide editor on specific pages.
+ *
+ */
+add_action( 'admin_init', 'hide_editor' );
+function hide_editor() {
+	// Get the Post ID.
+	$post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'];
+
+	if ( !isset( $post_id ) ) return;
+		// Hide the editor on the page titled 'Homepage'
+		$homepgname = get_the_title( $post_id );
+
+	if ( $homepgname == 'Home'){
+		remove_post_type_support('page', 'editor');
+	}
+
+	// Hide the editor on a page with a specific page template
+	// Get the name of the Page Template file.
+	$template_file = get_post_meta($post_id, '_wp_page_template', true);
+
+	if ( $template_file == 'my-page-template.php' ){ // the filename of the page template
+		remove_post_type_support('page', 'editor');
+  }
+}
